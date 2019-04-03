7bf29f1effbc2d286e50d2f531582ae4a1d0105c
Omar <omar.sidelkheir@gmail.com>
2019-04-04 01:18:35 +0200

    First report (top 3 articles) completed

--- log_report.py
+++ log_report.py
@@ -4,7 +4,13 @@ import datetime
 import psycopg2
 
 DBNAME = "news"
-query = "select * from authors;"
+
+top_3_articles_query = """select articles.title, article_views.nb_views
+ from articles join article_views on articles.slug = article_views.article
+ order by article_views.nb_views desc
+ limit 3;"""
+
+query = top_3_articles_query
 
 def get_data(query):
   """Executes SQL query and returns results in a list of tuples."""
@@ -16,6 +22,12 @@ def get_data(query):
 
   return results
 
+def top_3_articles_report(query_result):
+  with open('output.txt', mode='w') as f:
+    for entry in query_result:
+      f.write(entry[0] + ' - ' + format(entry[1], ',d') + ' views\n')
+
 
 if __name__ == "__main__":
   print(get_data(query))
+  top_3_articles_report(get_data(query))
---
+++ my_queries/article_views_VIEW.sql
@@ -0,0 +1,17 @@
+CREATE VIEW article_views AS
+SELECT
+    split_part(
+        path, '/', 3) AS article,
+    count(
+        *) AS nb_views
+FROM
+    log
+WHERE
+    method = 'GET'
+    AND status = '200 OK'
+    AND length(split_part (
+        path, '/', 3)) > 0
+GROUP BY
+    article
+ORDER BY
+    nb_views DESC
