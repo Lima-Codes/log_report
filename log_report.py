5b5e6452299a9f75b8183377652b5a294746f148
Omar <omar.sidelkheir@gmail.com>
2019-04-11 22:30:09 +0200

    PEP8 compliant formatting of log_report.py

--- log_report.py
+++ log_report.py
@@ -19,46 +19,51 @@ error_day_query = """select day, round(error_rate::numeric, 1)
 
 
 def get_data(query):
-  """Executes SQL query and returns results in a list of tuples."""
-  conn = psycopg2.connect(database=DBNAME)
-  cursor = conn.cursor()
-  cursor.execute(query)
-  results = cursor.fetchall()
-  conn.close()
-  return results
+    """Executes SQL query and returns results in a list of tuples."""
+    conn = psycopg2.connect(database=DBNAME)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
 
 
 def top_3_articles_report(query_result):
-  intro = "Top 3 most popular articles:\n"
-  with open('output.txt', mode='w') as f:
-    f.write(intro)
-    for entry in query_result:
-      f.write(entry[0] + ' - ' + format(entry[1], ',d') + ' views\n')
+    intro = "Top 3 most popular articles:\n"
+    with open('output.txt', mode='w') as f:
+        f.write(intro)
+        for entry in query_result:
+            f.write(entry[0] + ' - ' + format(entry[1], ',d') + ' views\n')
 
 
 def most_popular_authors_report(query_result):
-  intro = "\nMost popular authors:"
-  with open('output.txt', mode='a') as f:
-    f.write(intro)
-    for entry in query_result:
-      f.write('\n' + entry[0] + ' - ' + format(entry[1], ',d') + ' views')
+    intro = "\nMost popular authors:"
+    with open('output.txt', mode='a') as f:
+        f.write(intro)
+        for entry in query_result:
+            f.write(
+                '\n' + entry[0] + ' - ' + format(entry[1], ',d') + ' views'
+                    )
 
 
 def error_day_report(query_result):
-  intro = "\n\nDays with more than 1 percent request errors:"
-  with open('output.txt', mode='a') as f:
-    f.write(intro)
-    for entry in query_result:
-      f.write('\n' + entry[0].strftime("%d %B, %Y") + ' - ' + str(entry[1]) + ' % errors')
+    intro = "\n\nDays with more than 1 percent request errors:"
+    with open('output.txt', mode='a') as f:
+        f.write(intro)
+        for entry in query_result:
+            f.write(
+                '\n' + entry[0].strftime("%d %B, %Y") +
+                ' - ' + str(entry[1]) + ' % errors'
+                    )
 
 
 def main():
-  print("Running reports...")
-  top_3_articles_report(get_data(top_3_articles_query))
-  most_popular_authors_report(get_data(most_popular_authors_query))
-  error_day_report(get_data(error_day_query))
-  print("Reports ready")
+    print("Running reports...")
+    top_3_articles_report(get_data(top_3_articles_query))
+    most_popular_authors_report(get_data(most_popular_authors_query))
+    error_day_report(get_data(error_day_query))
+    print("Reports ready")
 
 
 if __name__ == "__main__":
-  main()
+    main()
